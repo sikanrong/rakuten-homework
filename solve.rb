7ef9e34bb44c6a7817d994ef3799dcc9ce55e9fa
sikanrong <sikanrong@gmail.com>
2014-12-12 16:42:58 +0100

    Almost done\! This adds the rest of the Node class, logic for bounding the cost matrices, logic for creating the node children, logic for calculating the base cost from the total reduction, etc. Also adds the beginning of the main solver logic. Not too much left to be done here before it's outputting solutions\!

--- solve.rb
+++ solve.rb
@@ -18,14 +18,19 @@ class Matrix
     reduction_ar = Array.new
     reduced_matrix_ar = self.to_a
     (0..(self.row_size-1)).each do |i|
-      row = self.row(i)     
-      reduction_ar.push(row.min) 
-      reduced_matrix_ar[i] = row.to_a.collect{|row_el| row_el - row.min}
+      row = self.row(i)
+      minval = row.min
+      reduction_ar.push(minval)
+      reduced_matrix_ar[i] = row.to_a.collect do |row_el|
+        next(Float::INFINITY) if (row_el.equal? Float::INFINITY or minval.equal? Float::INFINITY)
+
+        row_el - minval
+      end
     end
 
     return {
       :matrix=>Matrix[*reduced_matrix_ar], 
-      :reduction=>reduction_ar
+      :reduction=>reduction_ar.reject{|el| el.equal? Float::INFINITY}
     }
   end
   
@@ -38,13 +43,16 @@ class Matrix
       reduction_ar.push(minval) 
       (0..(self.row_size-1)).each do |i|
         val = reduced_matrix_ar[i][j]
+
+        next(Float::INFINITY) if (val.equal? Float::INFINITY or minval.equal? Float::INFINITY)
+
         reduced_matrix_ar[i][j] = val - minval
       end
     end
 
     return {
       :matrix=>Matrix[*reduced_matrix_ar], 
-      :reduction=>reduction_ar
+      :reduction=>reduction_ar.reject{|el| el.equal? Float::INFINITY}
     }
   end
   
@@ -65,42 +73,91 @@ class Node
   include Comparable
   
   attr_accessor :children #Node[]
-  attr_accessor :parent #Node[]
-  attr_accessor :city_id #integer
+  attr_accessor :parent #Node
+  attr_accessor :city_id #Integer
   
   attr_accessor :base_cost #Float
   attr_accessor :cost_matrix #Matrix
   attr_accessor :reduced_cost_matrix #Matrix
   
-  attr_accessor :blocked #Integer[]
-  
-    def initialize city_id, parent=nil
-      @city_id = city_id
-      @parent = parent
-      
-      if(not @parent.nil?)
-        #clone some data from the parent for this node...
-        @cost_matrix = parent.reduced_cost_matrix.clone
-        @blocked = parent.blocked.clone
-      end
-      
+  attr_accessor :visited_this_tour #Integer[]
+
+  def initialize city_id, parent=nil
+    @city_id = city_id
+    @parent = parent
+    @visited_this_tour = Array.new
+    @children = Array.new
+
+    if(not @parent.nil?)
+      self.set_cost_matrix(parent.reduced_cost_matrix.clone)
+      @visited_this_tour = parent.visited_this_tour.clone
       self.calculate_base_cost!
     end
-    
-    def calculate_base_cost!
+
+    @visited_this_tour << @city_id
+
+  end
+
+  def set_cost_matrix(parent_rcm)
+    @cost_matrix = parent_rcm
+
+    if(@parent) #only do this if not the ROOT node
+
+      rcm_2d_ar = @cost_matrix.to_a
+
+      #first set the row representing the parent city_id to infinity
+      #(represents that we can no longer go from this city since we just did)
+      rcm_2d_ar[@parent.city_id] = Array.new(@cost_matrix.row_size, Float::INFINITY)
+
+      #now set the column of the incoming city (this node) to infinity
+      #(represents that we can no longer come to this city since we just did)
+      (0..(@cost_matrix.row_size-1)).each do |i|
+        rcm_2d_ar[i][self.city_id] = Float::INFINITY
+      end
+
+      #finally, ensure that this entry in the first column is blocked
+      #(represents that we do not want to come back to the first node from this one)
+      rcm_2d_ar[self.city_id][1] = Float::INFINITY
+
+      @cost_matrix = Matrix[*rcm_2d_ar]
     end
- 
-    def <=>(another_node)
-      return self.base_cost <=> another_node.base_cost
+  end
+
+  def create_children!
+    all_cities = TSP.cities.keys
+    all_cities.reject!{|city_id| self.visited_this_tour.include?(city_id)}
+    all_cities.each do |city_id|
+      @children << Node.new(city_id, self)
     end
+  end
+
+  def calculate_base_cost!
+    result_obj = self.cost_matrix.reduction
+
+    @base_cost = result_obj[:total_reduction]
+    if(self.parent)
+      @base_cost += self.parent.base_cost + self.parent.reduced_cost_matrix[self.parent.city_id, self.city_id]
+    end
+
+    @reduced_cost_matrix = result_obj[:matrix]
+  end
+
+  def <=>(another_node)
+    return self.base_cost <=> another_node.base_cost
+  end
 end
 
 class TSP
-  
-  attr_accessor :cities
+  @@cities = {}
+
+  def self.cities
+    @@cities
+  end
+
+  attr_accessor :root_node
   attr_accessor :adj_matrix
   
-  def read_cities_from_stdin!
+  def read_cities_from_input!
     file = nil
 
     if(ARGV[0] and not ARGV[0].empty?)
@@ -111,30 +168,44 @@ class TSP
 
     file.each_with_index do |line, idx|
         line_ar = line.split("\t")
-        @cities[idx] = {:name=>line_ar[0], :location=>Vector[line_ar[1].to_f, line_ar[2].to_f]}
+        @@cities[idx] = {:name=>line_ar[0], :location=>Vector[line_ar[1].to_f, line_ar[2].to_f]}
     end
   end
   
   def calculate_adjacency_matrix!
-    @adj_matrix = Matrix.build(@cities.length) do |i,j|
+    @adj_matrix = Matrix.build(@@cities.length) do |i,j|
       if(i == j)
         next Float::INFINITY
       end
         
-      next (@cities[i][:location] - @cities[j][:location]).magnitude
+      next (@@cities[i][:location] - @@cities[j][:location]).magnitude
     end
   end
   
   def initialize
-    @cities ||= Hash.new
-    if(@cities.empty?)
-      self.read_cities_from_stdin!
+    @@cities ||= Hash.new
+    if(@@cities.empty?)
+      self.read_cities_from_input!
     end
-    
-    self.calculate_adjacency_matrix!
   end
   
   def solve!
+    #calculates the initial adjacency matrix
+    self.calculate_adjacency_matrix!
+
+    #create the root node, with city ID 0 (for Beijing)
+    self.root_node = Node.new(0)
+    self.root_node.set_cost_matrix(self.adj_matrix) #set the cost matrix to the adjacency matrix
+
+    #calculate the base cost manually (usually would happen during init but ROOT lacks a parent)
+    self.root_node.calculate_base_cost!
+    node = self.root_node
+
+    while(node.visited_this_tour.size < @@cities.size)
+      node.create_children!
+      children = node.children
+      raise children.min.inspect
+    end
 
   end
   
